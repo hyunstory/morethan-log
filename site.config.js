9c584f1b6570717d1d7ee56d4a73bc2967f09dbf
HYUNSEUNG LEE <hyunseunglee@MacBookAir-M2.local>
2025-07-23 00:45:19 +0900

    blog info update

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Hyunseung Lee",
+    name: "HyunseungLee",
     image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "3D Vision AI / Graphic Engineer",
     bio: "",
