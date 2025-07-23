4c52149a9a795980bb27b163d1e567319e7c5793
HYUNSEUNG LEE <hyunseunglee@MacBookAir-M2.local>
2025-07-24 00:58:49 +0900

    progress bar

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect } from "react"
+import React, { useState, useEffect, useRef, ReactNode } from "react"
 import { ThemeProvider } from "./ThemeProvider"
 import useScheme from "src/hooks/useScheme"
 import Header from "./Header"
@@ -39,25 +39,59 @@ import 'prismjs/components/prism-swift.js'
 import 'prismjs/components/prism-wasm.js'
 import 'prismjs/components/prism-yaml.js'
 import "prismjs/components/prism-go.js"
+import useThrottle from "src/hooks/useThrottle"
+import { useRouter } from "next/router"
 
 type Props = {
   children: ReactNode
 }
 
 const RootLayout = ({ children }: Props) => {
+  const router = useRouter()
+  const currentElementRef = useRef<HTMLDivElement | null>(null)
+
+  const [blogHeight, setBlogHeight] = useState(0)
+  const [throttleScrollY, setThrottleScrollY] = useState<number>(0)
   const [scheme] = useScheme()
   useGtagEffect()
+  const scrollThrottle = useThrottle(() => {
+    setThrottleScrollY(window.scrollY)
+  }, 100)
+
+  const getCurrentPercentage = () => {
+    if (router.asPath === "/") return 0
+
+    let percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
+
+    if (percentage >= 90) {
+      percentage = 100
+    } else {
+      percentage = Math.ceil((throttleScrollY / blogHeight) * 100)
+    }
+    return percentage
+  }
+
   useEffect(() => {
-    Prism.highlightAll();
-  }, []);
+    if (currentElementRef.current) {
+      const clientHeight = currentElementRef.current.clientHeight
+      setBlogHeight(clientHeight)
+    }
+
+    window.addEventListener("scroll", scrollThrottle)
+    return () => window.removeEventListener("scroll", scrollThrottle)
+  }, [throttleScrollY])
+
 
   return (
     <ThemeProvider scheme={scheme}>
       <Scripts />
       {/* // TODO: replace react query */}
       {/* {metaConfig.type !== "Paper" && <Header />} */}
-      <Header fullWidth={false} />
-      <StyledMain>{children}</StyledMain>
+      <Header
+        fullWidth={false}
+        readingProgress={blogHeight < 1200 ? 0 : getCurrentPercentage()}
+      />
+      <StyledMain ref={currentElementRef}>{children}</StyledMain>
     </ThemeProvider>
   )
 }
